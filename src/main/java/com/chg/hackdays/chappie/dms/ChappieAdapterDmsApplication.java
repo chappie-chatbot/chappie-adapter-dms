bac12698d02d6e2a3a8890214687fac0c2c892ee
Brandon Lyon <etherous@gmail.com>
2020-03-18 10:57:39 -0600

    Initial Commit

---
+++ src/main/java/com/chg/hackdays/chappie/dms/ChappieAdapterDmsApplication.java
@@ -0,0 +1,13 @@
+package com.chg.hackdays.chappie.dms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChappieAdapterDmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChappieAdapterDmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chg/hackdays/chappie/dms/ChappieAdapterDmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chg.hackdays.chappie.dms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChappieAdapterDmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
